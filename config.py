f73e19b6e076f801abc2735e3ecb4d6c4c7a5717
Joseph KJ <josephkj20@gmail.com>
2018-06-14 22:52:51 +0530

    Updates

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser('PGGAN')
 ## general settings.
 parser.add_argument('--train_data_root', type=str, default='/raid/datasets/img_align_celeba')
 parser.add_argument('--random_seed', type=int, default=int(time.time()))
-parser.add_argument('--n_gpu', type=int, default=1)             # for Multi-GPU training.
+parser.add_argument('--n_gpu', type=int, default=4)             # for Multi-GPU training.
 
 
 ## training parameters.
